79ecf363aa08ed7d3821829e1c36f2f4cf5405b4
volkovandr <16303028+volkovandr@users.noreply.github.com>
2021-04-26 22:26:35 +0200

    Reaction on mouse clicks

--- start.py
+++ start.py
@@ -7,11 +7,14 @@ speed = [2, 2]
 ball = pygame.image.load("graph/face1.png")
 ballrect = ball.get_rect()
 
-clock = pygame.time.Clock()
 
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode(size)
+
+    pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
+
+    clock = pygame.time.Clock()
 
     do_exit = False
     
@@ -26,12 +29,23 @@ if __name__ == "__main__":
         screen.blit(ball, ballrect)
         pygame.display.flip()
 
-        clock.tick_busy_loop(50)
+        clock.tick_busy_loop(30)
     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 do_exit = True
-
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    do_exit = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] < ballrect.left:
+                    speed[0] = speed[0] - 1
+                elif event.pos[0] > ballrect.right:
+                    speed[0] = speed[0] + 1
+                if event.pos[1] < ballrect.top:
+                    speed[1] = speed[1] - 1
+                elif event.pos[1] > ballrect.bottom:
+                    speed[1] = speed[1] + 1
     
 
 
